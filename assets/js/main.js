33bc6b359ba211e5d3b77584705cdc441d23a9f1
Nguyen Minh Hieu <86197383+HieuNguyenDev@users.noreply.github.com>
2022-12-07 21:06:58 +0700

    responsive website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,69 @@
+// Modal
 const modalElement = document.querySelector('.modal');
 const modalContainer = document.querySelector('.modal-container');
 const modalCloseBtn = document.querySelector('.js-modal-container__close');
 const buyTicketBtns = document.querySelectorAll('.js-buy-ticket-btn');
 
-for (const buyTicketBtn of buyTicketBtns) {
-    buyTicketBtn.addEventListener('click', showModal)
+// Responsive Mobile Menu
+const header = document.querySelector('#header');
+const menuBtn = document.querySelector('.menu');
+const menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+
+
+// Handle Buy Tickets
+function handleBuyTickets() {
+    for (const buyTicketBtn of buyTicketBtns) {
+        buyTicketBtn.addEventListener('click', showModal)
+    }
+    
+    modalContainer.addEventListener('click', (event) => {
+        event.stopPropagation();
+    })
+    
+    function showModal() {
+        modalElement.classList.add('open');
+    }
+    
+    function hiddenModal() {
+        modalElement.classList.remove('open');
+    }
+    
+    modalCloseBtn.addEventListener('click', hiddenModal);
+    modalElement.addEventListener('click', hiddenModal);
 }
 
-modalContainer.addEventListener('click', (event) => {
-    event.stopPropagation();
-})
+// Handle Mobile Menu
+function handleMobileMenu() {
+    var headerHeight = header.clientHeight;
 
-function showModal() {
-    modalElement.classList.add('open');
+    // Show menu when click
+    menuBtn.addEventListener('click', () => {
+        var isClosed = header.clientHeight === headerHeight;
+        if (isClosed) {
+            header.style.height = 'auto'
+        } else {
+            header.style.height = null
+        }
+    })
+
+    // Hidden menu when click
+    for (let menuItem of menuItems) {
+        menuItem.onclick = (event) => {
+            var isParentMenu = menuItem.nextElementSibling && menuItem.nextElementSibling.classList.contains('sub-nav');
+
+            if (isParentMenu) {
+                event.preventDefault();
+            } else {
+                header.style.height = null
+            }
+        }
+    }
 }
 
-function hiddenModal() {
-    modalElement.classList.remove('open');
+function start() {
+    handleBuyTickets();
+    handleMobileMenu();
 }
 
-modalCloseBtn.addEventListener('click', hiddenModal);
-modalElement.addEventListener('click', hiddenModal);
+start();
+
